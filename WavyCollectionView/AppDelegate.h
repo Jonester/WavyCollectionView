1ff5b784d18949ab57c9dfce24416e8c968c65fc
Jonester <chris.jones78@gmail.com>
2017-01-29 16:27:31 -0500

    Initial Commit

---
+++ WavyCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavyCollectionView
+//
+//  Created by Chris Jones on 2017-01-29.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
